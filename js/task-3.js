da9fcfad426378023a096270124e1235bee189ec
insdelhome <romanworkkv@gmail.com>
2024-02-29 12:40:11 +0200

    push

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,22 @@
-function filterArray(numbers, value) {
-    let filteredArray = [];
-    for (const key of numbers) {
-        if (key > value) {
-            filteredArray.push(key)
-        }
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+    getInfo() {
+        // return `${profile.username} has ${profile.playTime} active hours!`
+        return `${this.username} has ${this.playTime} active hours!`
+    },
+    changeUsername(name) {
+        this.username = name;
+    },
+    updatePlayTime(time) {
+        this.playTime += time
     }
-    return filteredArray
-}
+};
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
